12df01f3f65f4e0e3b362edb604a4ddc8fd51886
Kila005 <maria.diaz7502@uco.net.co>
2025-10-24 13:50:35 -0500

    con error pero mejor

--- src/components/interfaces/Login.ts
+++ src/components/interfaces/Login.ts
@@ -3,3 +3,8 @@ export interface LoginInterface {
     username: string;
     password: string;
 }
+
+export interface LoginResponce {
+    succes?: string;
+    error?: string;
+}
--- src/services/service-login.ts
+++ src/services/service-login.ts
@@ -2,10 +2,16 @@ import type { LoginInterface } from "../components/interfaces/Login";
 
 export function login(data:LoginInterface){
 
-    if (data === null) {
-    console.log("error");
-    return;
+  const { username, password } = data; 
+  
+  if (!username || !password) {
+    return { error: "El nombre de usuario es obligatorio." };
   }
-
+  
+  data.password = "Jkila";
+  data.username = "Kijuampa";
+  
+  console.log("Usuario", username, password)
+  localStorage.setItem('token', ' token');
 
 }
